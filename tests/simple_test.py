7c7d7a2047153244e3a51ce1524f0270d682238f
iraklis7 <100917652+iraklis7@users.noreply.github.com>
2025-12-18 09:46:32 +0200

    Add files via upload
    added a simple test

---
+++ tests/simple_test.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from src.utility_bill_processor.utility_proc import Utility_Bill_Processor
+
+def simple_test():
+    total_differences = 0
+    invoices_path = "./invoices/"
+    expected_values_path = "./expected"
+    utility_bill = invoices_path + "ElectricityInvoice_2025-11-04.pdf"
+    processor = Utility_Bill_Processor(output_dir="../output")
+
+
+    print("Processing file: " + utility_bill)
+
+    try:
+        parse_res   = processor.parse(filelist[i])
+        schema      = processor.get_schema(parse_res.markdown)
+        extract_res = processor.extract(parse_res.markdown, schema)
+    except Exception as e:
+        print("Error processing file " + utility_bill + ": " + str(e))
+        exit(1)
+
+    ext_values = extract_res.extraction
+    print("{:<30} {:<30} ".format('FIELD', 'VALUE'))
+
+def main():
+    simple_test()
+
+if __name__ == "__main__":
+    main()
