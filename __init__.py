c2982b68f950eca9a3aec438684361797d1ccb48
iraklis7 <iraklis.diamantidis@gmail.com>
2025-12-23 18:07:25 +0200

    another try 2

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-from importlib.resources import files
-#data_text = print(files("ubp").joinpath("output/*.json").read_text())
-print(files("iraklis7_ubp"))
+#from importlib.resources import files
+#data_text = print(files("iraklis7_ubp").joinpath("output/*.json").read_text())
+#print(data_text)
       
       
